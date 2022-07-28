dee9dfab3526f1b67170244e6fbbf73cccf140d9
xAzozz7 <110201108+xAzozz7@users.noreply.github.com>
2022-07-28 23:52:28 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,192 @@
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
+
+const btnbar = document.querySelector(".btn-bar");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    btnbar.classList.add("active");
+  } else {
+    btnbar.classList.remove("active");
+  }
+})
+
+function myBarAZiz() {
+  var elem = document.getElementById("myBarAZiz");   
+  var width = 10;
+  var id = setInterval(frame, 20);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
+
+function myBarMuzzmil() {
+    var elem = document.getElementById("myBarMuzzmil");   
+    var width = 10;
+    var id = setInterval(frame, 20);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+        elem.innerHTML = width * 1  + '%';
+      }
+    }
+  }
+  function myBarYazeed() {
+    var elem = document.getElementById("myBarYazeed");   
+    var width = 10;
+    var id = setInterval(frame, 20);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+        elem.innerHTML = width * 1  + '%';
+      }
+    }
+  }
+  function myBarsaeed() {
+    var elem = document.getElementById("myBarsaeed");   
+    var width = 10;
+    var id = setInterval(frame, 20);
+    function frame() {
+      if (width >= 95) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+        elem.innerHTML = width * 1  + '%';
+      }
+    }
+  }
+  function myBarKHALED() {
+    var elem = document.getElementById("myBarKHALED");   
+    var width = 10;
+    var id = setInterval(frame, 20);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+        elem.innerHTML = width * 1  + '%';
+      }
+    }
+  }
+  
+  function myBarArthur() {
+    var elem = document.getElementById("myBarArthur");   
+    var width = 10;
+    var id = setInterval(frame, 20);
+    function frame() {
+      if (width >= 90) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+        elem.innerHTML = width * 1  + '%';
+      }
+    }
+  }
+  function myBarAli() {
+    var elem = document.getElementById("myBarAli");   
+    var width = 10;
+    var id = setInterval(frame, 20);
+    function frame() {
+      if (width >= 97) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+        elem.innerHTML = width * 1  + '%';
+      }
+    }
+  }
+  function myBaralhpa() {
+    var elem = document.getElementById("myBaralhpa");   
+    var width = 10;
+    var id = setInterval(frame, 20);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+        elem.innerHTML = width * 1  + '%';
+      }
+    }
+  }
+  function myBaryousef() {
+    var elem = document.getElementById("myBaryousef");   
+    var width = 10;
+    var id = setInterval(frame, 20);
+    function frame() {
+      if (width >= 40) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+        elem.innerHTML = width * 1  + '%';
+      }
+    }
+  }
+  function myBarNorh() {
+    var elem = document.getElementById("myBarNorh");   
+    var width = 10;
+    var id = setInterval(frame, 20);
+    function frame() {
+      if (width >= 60) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+        elem.innerHTML = width * 1  + '%';
+      }
+    }
+  }
+  function myBarSalem() {
+    var elem = document.getElementById("myBarSalem");   
+    var width = 10;
+    var id = setInterval(frame, 20);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+        elem.innerHTML = width * 1  + '%';
+      }
+    }
+  }
+  function myBarkillua() {
+    var elem = document.getElementById("myBarkillua");   
+    var width = 10;
+    var id = setInterval(frame, 20);
+    function frame() {
+      if (width >= 80) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+        elem.innerHTML = width * 1  + '%';
+      }
+    }
+  }
+
